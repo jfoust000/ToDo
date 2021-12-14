bed75ff4d7a2fb75b93f839b20417a26a0ad80d6
jfoust000 <joel.foust@yahoo.com>
2021-12-13 23:52:03 -0500

    Added Local Storage Functionality for adding To Dos and Categories. Still need to remove storage for deleted ToDos.

--- ToDo.js
+++ ToDo.js
@@ -2,13 +2,20 @@ todoMain();
 
 function todoMain(){
 
+    const DEFAULT_CATEGORY_FILTER = "All Categories";
+
     let inputElement,
         categoryElement,
         addButton,
-        selectElement;
+        selectElement,
+        todoListStorageData = [];
+
 
     getElements();
     addListeners();
+    load();
+    renderRows();
+    upDateSelectOptions();
 
     function getElements(){
         inputElement = document.getElementsByTagName("input")[0];
@@ -29,6 +36,103 @@ function todoMain(){
 
         let inputValue2 = categoryElement.value;
         categoryElement.value = "";
+
+
+
+        todoListStorageData.push({
+            todo: inputValue,
+            category: inputValue2,
+        });
+        renderRow(todoListStorageData);
+        save();
+
+        upDateSelectOptions();
+
+    }
+
+    function filterEntries() {
+        let selection = selectElement.value;
+
+        if(selection === DEFAULT_CATEGORY_FILTER){
+
+            let rows = document.getElementsByTagName("tr");
+
+            Array.from(rows).forEach((row)=>{
+                row.style.display = "";
+            });
+        }else{
+        let rows = document.getElementsByTagName("tr");
+
+        Array.from(rows).forEach((row, index)=>{
+            if(index===0){
+                return;
+            }
+            let category = row.getElementsByTagName("td")[2].innerText;
+            if (category === selectElement.value){
+                row.style.display = "";
+            }else {
+                row.style.display = "none";
+            }
+        });
+        }
+
+    }
+    function upDateSelectOptions(){
+        let options = [];
+
+        let rows = document.getElementsByTagName("tr");
+
+        Array.from(rows).forEach((row, index)=> {
+            if (index === 0) {
+                return;
+            }
+            let category = row.getElementsByTagName("td")[2].innerText;
+
+                options.push(category);
+
+
+        });
+
+        let optionsSet = new Set(options);
+
+
+        //empty options
+        selectElement.innerHTML = "";
+
+        let newOptionElement = document.createElement('option');
+        newOptionElement.value = DEFAULT_CATEGORY_FILTER;
+        newOptionElement.innerText = DEFAULT_CATEGORY_FILTER;
+        selectElement.appendChild(newOptionElement);
+
+        for (let option of optionsSet){
+            let newOptionElement = document.createElement('option');
+            newOptionElement.value = option;
+            newOptionElement.innerText = option;
+            selectElement.appendChild(newOptionElement);
+        }
+        filterEntries();
+
+
+    }
+    function save(){
+        let stringified = JSON.stringify(todoListStorageData);
+        localStorage.setItem("todoListStorageData", stringified);
+    }
+    function load(){
+
+        let retrievedStorageData = localStorage.getItem("todoListStorageData");
+        todoListStorageData = JSON.parse(retrievedStorageData);
+        if (todoListStorageData === null){
+            todoListStorageData = [];
+        }
+
+    }
+    function renderRows(){
+    todoListStorageData.forEach(todoObj => {
+        renderRow(todoObj);
+    })
+    }
+    function renderRow({todo: inputValue, category: inputValue2}){
 
         //Add a new row
 
@@ -69,39 +173,12 @@ function todoMain(){
 
         function deleteItem(){
             trElement.remove();
+
+            upDateSelectOptions();
         }
 
         function done(){
             trElement.classList.toggle("strike");
         }
-
-    }
-
-    function filterEntries() {
-        let selection = selectElement.value;
-
-        if(selection === "All Categories"){
-
-            let rows = document.getElementsByTagName("tr");
-
-            Array.from(rows).forEach((row)=>{
-                row.style.display = "";
-            });
-        }else{
-        let rows = document.getElementsByTagName("tr");
-
-        Array.from(rows).forEach((row, index)=>{
-            if(index===0){
-                return;
-            }
-            let category = row.getElementsByTagName("td")[2].innerText;
-            if (category === selectElement.value){
-                row.style.display = "";
-            }else {
-                row.style.display = "none";
-            }
-        });
-        }
-
     }
 }
